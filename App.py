d97ddc389d1f9d56936364d86b14320f311792ec
florianmolinadl-hue <florianmolinadl@gmail.com>
2026-04-29 16:09:46 +0200

    App.py

--- App.py
+++ App.py
@@ -25,8 +25,6 @@ def send():
 @app.route("/get", methods=["GET"])
 def get():
     return jsonify(messages)
-
-port import os
-
+    
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
